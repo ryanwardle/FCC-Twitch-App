85fe519012d9de36a42ebb70a83028228ac6c602
unknown <wardle2106@gmail.com>
2018-05-10 12:59:15 -0400

    javascript branch

--- script.js
+++ script.js
@@ -4,18 +4,43 @@ $(document).ready(function(){
 
   //USER BEING USED FOR TEST PURPOSES, WILL PROBABLY HAVE TO USE FOR LOOP, TO LOOP THROUGH USERS
   //THEN DISPLAY INFORMATION BASED ON JSON DATA
-  let user =  'freecodecamp';
+  let user =  userArray[i];
   let userArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
   let url = 'https://wind-bow.gomix.me/twitch-api' + '/channels/' + user + '?callback=?';
+  let url2 = 'https://wind-bow.gomix.me/twitch-api' + '/streams/' + user + '?callback=?';
+  let channelName, logo, onOff;
 
   $.getJSON(url, function(json){
-    let jsonString = JSON.stringify(json)
-    let channelName = JSON.stringify(json.display_name);
-    console.log(jsonString);
+    //let jsonString = JSON.stringify(json)
+    channelName = JSON.stringify(json.display_name); //sTREAMER DISPLAY NAME
+    //console.log(jsonString);
     console.log(channelName);
-
-      let logo = json.logo; //image link for logo
+    logo = json.logo; //image link for logo
   });
+
+//CHECK IF STREAMER IS ONLINE
+  $.getJSON(url2, function(json){
+    if (json.stream !== null) {
+      console.log('online');
+      onOff = 'online';
+    }else {
+      console.log('offline');
+      onOff = 'offline';
+    }
+
+    //TRYING TO INSERT INTO DOM, MAY NEED TO PUT EVERYTHING INSIDE OF TIS LOOP
+    for (let i = 0; i < userArray.length; i++) {
+      let newDiv = document.createElement('div');
+      let content = document.createTextNode(channelName);
+      newDiv.appendChild(content);
+      document.body.appendChild(newDiv);
+    }
+
+  });
+
+  //NEED TO DISPLAY ALL STREAMS WHEN PAGE LOADS
+  //DISPLAY ONLINE ONLY WHEN ONLINE IS CLICKED
+  //DISPLAY OFFLINE ONLY WHEN OFFLINE IS CLICKED
 
 
 });
---
+++ script2.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  let channelName, logo, onOff;
+  let userArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+    for (let i = 0; i < 2; i++) {   //userArray.length
+      let user =  userArray[i];
+
+      let url = 'https://wind-bow.gomix.me/twitch-api' + '/channels/' + user + '?callback=?';
+      let url2 = 'https://wind-bow.gomix.me/twitch-api' + '/streams/' + user + '?callback=?';
+
+
+      //SEEMS LIKE DATA IS NOT COMING BACK IN ORDER AND IS GIVING PROBLEMS TO DISPLAY
+      $.getJSON(url, function(json){
+        //let jsonString = JSON.stringify(json)
+          //console.log(jsonString);
+
+        channelName = JSON.stringify(json.display_name);
+        console.log(channelName);
+        channelName = channelName.replace(/"/, '');
+        channelName = channelName.replace(/"/, '');
+        console.log(channelName);
+        logo = json.logo; //image link for logo
+      });
+
+    //CHECK IF STREAMER IS ONLINE
+      $.getJSON(url2, function(json){
+        if (json.stream !== null) {
+          console.log('online');
+          onOff = 'online';
+        }else {
+          console.log('offline');
+          onOff = 'offline';
+        }
+
+      let newDiv = document.createElement('div');
+      let content = document.createTextNode(channelName);
+      newDiv.appendChild(content);
+      document.body.appendChild(newDiv);
+
+  });
+}
+  //NEED TO DISPLAY ALL STREAMS WHEN PAGE LOADS
+  //DISPLAY ONLINE ONLY WHEN ONLINE IS CLICKED
+  //DISPLAY OFFLINE ONLY WHEN OFFLINE IS CLICKED
+
+});
