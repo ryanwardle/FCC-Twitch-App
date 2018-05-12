163bf74c4ca588f0b5a0939920bc07677ef324be
ryanwardle <37915565+ryanwardle@users.noreply.github.com>
2018-05-11 21:47:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function(){
 
+  //****************CURRENTLY NEED TO FIGURE OUT HOW TO INSERT DATA INTO DOM, NOT WORKING OR NEED TO FIGURE OUT
+  // **** HOW TO ACCURATELY DISPLAY SOME OF THE DATA AND THEN BUILD FROM THERE
 
   let userArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
@@ -13,13 +15,14 @@ $(document).ready(function(){
 
       //SEEMS LIKE DATA IS NOT COMING BACK IN ORDER AND IS GIVING PROBLEMS TO DISPLAY
       $.getJSON(url, function(json){
-        let jsonString = JSON.stringify(json)
-          console.log(jsonString);
+        //let jsonString = JSON.stringify(json)
+          //console.log(jsonString);
         channelName = JSON.stringify(json.display_name);
         channelName = channelName.replace(/"/, '');
         channelName = channelName.replace(/"/, '');
         console.log(channelName);
         logo = json.logo; //image link for logo
+
       });
 
     //CHECK IF STREAMER IS ONLINE
@@ -31,13 +34,13 @@ $(document).ready(function(){
           console.log('offline');
           onOff = 'offline';
         }
+        let newDiv = document.createElement('div');
+        let content = document.createTextNode(channelName + ' is currently ' + onOff); //will change diplay just using to test
+        newDiv.appendChild(content);
+        document.body.appendChild(newDiv);
   });
 
   //MAY NEED TO ADD CLASSES ETC.. TO HELP STYLE DIV
-  let newDiv = document.createElement('div');
-  let content = document.createTextNode(channelName)// + ' is currently ' + onOff); //will change diplay just using to test
-  newDiv.appendChild(content);
-  document.body.appendChild(newDiv);
 }
   //NEED TO DISPLAY ALL STREAMS WHEN PAGE LOADS
   //DISPLAY ONLINE ONLY WHEN ONLINE IS CLICKED
