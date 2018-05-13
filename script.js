56f520f564087e2d86b4bdbd9bb53a1eff22a53a
unknown <wardle2106@gmail.com>
2018-05-13 12:50:48 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Good eaample https://codepen.io/Olegukoff/full/ZLdWRv
 
+//HAVE NAME DISPLAYED, NEED TO FIGURE OUT HOW TO ADD TO DISPLAY AND THEN I NEED TO STYLE THER DISPLAY
+
 $(document).ready(function(){
 
   let userArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
@@ -21,14 +23,11 @@ $(document).ready(function(){
           channelName = channelName.replace(/"/, '');
           console.log(channelName);
           logo = json.logo;
-          addData();
+          //addData();
         }
       })
 
-
-
     //CHECK IF STREAMER IS ONLINE
-
     $.ajax({
       type: 'GET',
       url: 'https://wind-bow.gomix.me/twitch-api' + '/streams/' + user + '?callback=?',
@@ -39,20 +38,37 @@ $(document).ready(function(){
         }else {
           onOff = 'offline';
         }
-        //addData(' is currently ' + onOff);
+        addData(); //(' is currently ' + onOff);
       }
     })
 
+  function addData(){
+  let newUl = document.createElement('ul');
+  newUl.setAttribute('id', i);
+  document.getElementById('streamers').appendChild(newUl);
+  let li1 = document.createElement('img');
+  li1.setAttribute('src', logo);
+  let li2 = document.createElement('li');
+  let li3 = document.createElement('li');
+
+  //let liContent1 = document.createTextNode(logo);
+  //let liContent2 = document.createTextNode(channelName);
+  //let liContent3 = document.createTextNode(onOff);
 
 
+  //li1.innerText = logo;
+  li2.innerText = channelName;
+  li3.innerText = onOff;
 
-  function addData(newData){
-  let newDiv = document.createElement('div');
-  let content = document.createTextNode(channelName)// + newData) //+ ' is currently ' + onOff); //will change diplay just using to test
-  newDiv.appendChild(content);
-  document.body.appendChild(newDiv);
+  document.getElementById(i).appendChild(li1);
+  document.getElementById(i).appendChild(li2);
+  document.getElementById(i).appendChild(li3);
+
 }
 
+//GOING TO TRY TO WRITE WHOLE FUNCTION THEN CALL AT END
+
+  //addData();
 }
 
 });
