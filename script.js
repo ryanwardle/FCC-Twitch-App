066d615d3b153ac4e04d845c33b3017ed59846f3
unknown <wardle2106@gmail.com>
2018-05-13 14:48:18 -0400

    update

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 // Good eaample https://codepen.io/Olegukoff/full/ZLdWRv
 
-//HAVE NAME DISPLAYED, NEED TO FIGURE OUT HOW TO ADD TO DISPLAY AND THEN I NEED TO STYLE THER DISPLAY
+//************INSTEAD OF WRITING FUNCTIONI, WILL TRY TO PUT DIRECTLY INTO DOM FROM AJAX SUCCES FUNCTION
 
 $(document).ready(function(){
 
   let userArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
     for (let i = 0; i < userArray.length; i++) {
+
       let channelName, logo, onOff;
       let user =  userArray[i];
       let url = 'https://wind-bow.gomix.me/twitch-api' + '/channels/' + user + '?callback=?';
       let url2 = 'https://wind-bow.gomix.me/twitch-api' + '/streams/' + user + '?callback=?';
 
       //GET DISPLAY NAME FOR CHANNEL
+
       $.ajax({
         type: 'GET',
         url: 'https://wind-bow.gomix.me/twitch-api' + '/channels/' + user + '?callback=?',
@@ -21,13 +23,16 @@ $(document).ready(function(){
           channelName = JSON.stringify(json.display_name);
           channelName = channelName.replace(/"/, '');
           channelName = channelName.replace(/"/, '');
-          console.log(channelName);
           logo = json.logo;
-          //addData();
+
+          //addData(); *******READ TOP NOTES*******
         }
       })
 
+
+
     //CHECK IF STREAMER IS ONLINE
+
     $.ajax({
       type: 'GET',
       url: 'https://wind-bow.gomix.me/twitch-api' + '/streams/' + user + '?callback=?',
@@ -38,37 +43,39 @@ $(document).ready(function(){
         }else {
           onOff = 'offline';
         }
-        addData(); //(' is currently ' + onOff);
+        //addData();   ************READ TOP NOTES*******
       }
     })
 
-  function addData(){
-  let newUl = document.createElement('ul');
-  newUl.setAttribute('id', i);
-  document.getElementById('streamers').appendChild(newUl);
-  let li1 = document.createElement('img');
-  li1.setAttribute('src', logo);
-  let li2 = document.createElement('li');
-  let li3 = document.createElement('li');
 
-  //let liContent1 = document.createTextNode(logo);
-  //let liContent2 = document.createTextNode(channelName);
-  //let liContent3 = document.createTextNode(onOff);
+//*********READ TOP NOTES***********
+function addData(){
+let newUl = document.createElement('ul');
+newUl.setAttribute('id', 'list' + i);
 
+document.getElementById('streamers').appendChild(newUl);
 
-  //li1.innerText = logo;
-  li2.innerText = channelName;
-  li3.innerText = onOff;
+let li1 = document.createElement('li');
+li1.setAttribute('id', 'img' + i);
 
-  document.getElementById(i).appendChild(li1);
-  document.getElementById(i).appendChild(li2);
-  document.getElementById(i).appendChild(li3);
+let img = document.createElement('img');
+img.setAttribute('src', logo);
+img.setAttribute('alt', "Streamer's Logo");
+
+let li2 = document.createElement('li');
+let li3 = document.createElement('li');
+
+li2.innerText = channelName;
+li3.innerText = onOff;
+
+document.getElementById('list' + i).appendChild(li1);
+document.getElementById('img' + i).appendChild(img);
+document.getElementById('list' + i).appendChild(li2);
+document.getElementById('list' + i).appendChild(li3);
 
 }
 
-//GOING TO TRY TO WRITE WHOLE FUNCTION THEN CALL AT END
 
-  //addData();
 }
 
 });
