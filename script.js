7b8f04224a36e211bbd02781faf27e734bc625bc
unknown <wardle2106@gmail.com>
2018-05-14 13:50:38 -0400

    update

--- script.js
+++ script.js
@@ -25,56 +25,45 @@ $(document).ready(function(){
           channelName = channelName.replace(/"/, '');
           logo = json.logo;
 
-          //addData(); *******READ TOP NOTES*******
+          let newUl = document.createElement('ul');
+          newUl.setAttribute('id', 'list' + i);
+
+          document.getElementById('streamers').appendChild(newUl);
+
+          let li1 = document.createElement('li');
+          li1.setAttribute('id', 'img' + i);
+
+          let img = document.createElement('img');
+          img.setAttribute('src', logo);
+          img.setAttribute('alt', "Streamer's Logo");
+
+          let li2 = document.createElement('li');
+
+          li2.innerText = channelName;
+
+          document.getElementById('list' + i).appendChild(li1);
+          document.getElementById('img' + i).appendChild(img);
+          document.getElementById('list' + i).appendChild(li2);
+
+          //CHECK IF STREAMER IS ONLINE
+          $.ajax({
+            type: 'GET',
+            url: 'https://wind-bow.gomix.me/twitch-api' + '/streams/' + user + '?callback=?',
+            dataType: 'json',
+            success: function(json){
+              if (json.stream !== null) {
+                onOff = 'online';
+              }else {
+                onOff = 'offline';
+              }
+
+              let li3 = document.createElement('li');
+              li3.innerText = onOff;
+              document.getElementById('list' + i).appendChild(li3);
+            }
+          })
         }
       })
-
-
-
-    //CHECK IF STREAMER IS ONLINE
-
-    $.ajax({
-      type: 'GET',
-      url: 'https://wind-bow.gomix.me/twitch-api' + '/streams/' + user + '?callback=?',
-      dataType: 'json',
-      success: function(json){
-        if (json.stream !== null) {
-          onOff = 'online';
-        }else {
-          onOff = 'offline';
-        }
-        //addData();   ************READ TOP NOTES*******
-      }
-    })
-
-
-//*********READ TOP NOTES***********
-function addData(){
-let newUl = document.createElement('ul');
-newUl.setAttribute('id', 'list' + i);
-
-document.getElementById('streamers').appendChild(newUl);
-
-let li1 = document.createElement('li');
-li1.setAttribute('id', 'img' + i);
-
-let img = document.createElement('img');
-img.setAttribute('src', logo);
-img.setAttribute('alt', "Streamer's Logo");
-
-let li2 = document.createElement('li');
-let li3 = document.createElement('li');
-
-li2.innerText = channelName;
-li3.innerText = onOff;
-
-document.getElementById('list' + i).appendChild(li1);
-document.getElementById('img' + i).appendChild(img);
-document.getElementById('list' + i).appendChild(li2);
-document.getElementById('list' + i).appendChild(li3);
-
-}
-
 
 }
 
