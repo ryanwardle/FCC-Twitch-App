c36f4b3691d1d3fb11a3b0ae9494dc14b5e60ee4
unknown <wardle2106@gmail.com>
2018-05-14 15:08:58 -0400

    update

--- script.js
+++ script.js
@@ -13,57 +13,48 @@ $(document).ready(function(){
       let url = 'https://wind-bow.gomix.me/twitch-api' + '/channels/' + user + '?callback=?';
       let url2 = 'https://wind-bow.gomix.me/twitch-api' + '/streams/' + user + '?callback=?';
 
-      //GET DISPLAY NAME FOR CHANNEL
+      //GET DISPLAY NAME AND LOGO FOR CHANNEL
+      $.getJSON(url).then(function(json){
+        channelName = JSON.stringify(json.display_name);
+        channelName = channelName.replace(/"/, '');
+        channelName = channelName.replace(/"/, '');
+        logo = json.logo;
 
-      $.ajax({
-        type: 'GET',
-        url: 'https://wind-bow.gomix.me/twitch-api' + '/channels/' + user + '?callback=?',
-        dataType: 'json',
-        success: function(json){
-          channelName = JSON.stringify(json.display_name);
-          channelName = channelName.replace(/"/, '');
-          channelName = channelName.replace(/"/, '');
-          logo = json.logo;
+        //DISPLAY DATA BY MAKING AND INSERTING LIST INTO DOM
+        let newUl = document.createElement('ul');
+        newUl.setAttribute('id', 'list' + i);
 
-          let newUl = document.createElement('ul');
-          newUl.setAttribute('id', 'list' + i);
+        document.getElementById('streamers').appendChild(newUl);
 
-          document.getElementById('streamers').appendChild(newUl);
+        let li1 = document.createElement('li');
+        li1.setAttribute('id', 'img' + i);
 
-          let li1 = document.createElement('li');
-          li1.setAttribute('id', 'img' + i);
+        let img = document.createElement('img');
+        img.setAttribute('src', logo);
+        img.setAttribute('alt', "Streamer's Logo");
 
-          let img = document.createElement('img');
-          img.setAttribute('src', logo);
-          img.setAttribute('alt', "Streamer's Logo");
+        let li2 = document.createElement('li');
 
-          let li2 = document.createElement('li');
+        li2.innerText = channelName;
 
-          li2.innerText = channelName;
+        document.getElementById('list' + i).appendChild(li1);
+        document.getElementById('img' + i).appendChild(img);
+        document.getElementById('list' + i).appendChild(li2);
 
-          document.getElementById('list' + i).appendChild(li1);
-          document.getElementById('img' + i).appendChild(img);
-          document.getElementById('list' + i).appendChild(li2);
-
-          //CHECK IF STREAMER IS ONLINE
-          $.ajax({
-            type: 'GET',
-            url: 'https://wind-bow.gomix.me/twitch-api' + '/streams/' + user + '?callback=?',
-            dataType: 'json',
-            success: function(json){
-              if (json.stream !== null) {
-                onOff = 'online';
-              }else {
-                onOff = 'offline';
-              }
-
-              let li3 = document.createElement('li');
-              li3.innerText = onOff;
-              document.getElementById('list' + i).appendChild(li3);
-            }
-          })
+      //GET AND DISPLAY ONLINE OR OFFLINE STATUS
+        return $.getJSON(url2)
+      }).then(function(json){
+        if (json.stream !== null) {
+          onOff = 'online';
+        }else {
+          onOff = 'offline';
         }
-      })
+
+        //DISPLAY BY ADDING LIST ITEM
+        let li3 = document.createElement('li');
+        li3.innerText = onOff;
+        document.getElementById('list' + i).appendChild(li3);
+      });
 
 }
 
